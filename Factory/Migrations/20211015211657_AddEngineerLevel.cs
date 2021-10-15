a7686ac87b81192f482aafebf23686d348cc46d0
Melissa <melissa.scmi@gmail.com>
2021-10-15 14:25:25 -0700

    update Engineers with skill levels

---
+++ Factory/Migrations/20211015211657_AddEngineerLevel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class AddEngineerLevel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "Engineers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Engineers");
+        }
+    }
+}
